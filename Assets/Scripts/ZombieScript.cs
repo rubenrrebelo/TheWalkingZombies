9a62cd955b11ad67a5546c291578d18dbf7daa37
Élvio <ak4iwa@gmail.com>
2014-04-15 19:08:20 +0100

    omgdeixameempaz

---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ZombieScript : MonoBehaviour {
+
+	private float _healthLevel;
+	private float _movSpeed;
+	private float _visionRange;
+	private float _attDamage;
+	private float _attRange;
+
+	private List<GameObject> _survivorsInSight;
+
+
+
+	
+	// Use this for initialization
+	void Start () {
+		_healthLevel = 10.0f;
+		_movSpeed = 5.0f;
+		_visionRange = 10.0f;
+		_attDamage = 5.0f;
+		_attRange = 1.0f;
+
+		_survivorsInSight = new List<GameObject>();
+
+		//TODO: Inicializar o radius do VisionRange collider do zombie com o valor _visionRange
+	}
+	
+	//Actuadores-------------------------------------------------------------------
+	//Attack-Survivor
+	
+	
+	//Random-Move
+	private void randomMove(){}
+	
+	//Sensores---------------------------------------------------------------------
+	//See-Survivor (posição do survivor mais próx)
+
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag.Equals("Survivor")){
+			_survivorsInSight.Add(other.gameObject);
+		}
+	}
+	void OnTriggerExit (Collider other){
+		_survivorsInSight.Remove(other.gameObject);
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		randomMove();
+
+		if(_survivorsInSight.Count != 0){
+			foreach(GameObject survivor in _survivorsInSight){
+				Debug.Log( "Zombie: " + survivor.gameObject.transform.position);
+			}
+		}
+		
+		
+	}
+}
